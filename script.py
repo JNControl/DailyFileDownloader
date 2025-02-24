52bc44ed4ff999b88c2f06cd22b661ab5dcb8cc3
JNControl <53433058+JNControl@users.noreply.github.com>
2025-02-24 23:28:41 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,12 @@ import requests
 import os
 
 # Nahraď těmito URL adresami, co chceš stahovat
-urls = ["https://example.com/file1.txt", "https://example.com/file2.txt"]
+# urls = ["https://example.com/file1.txt", "https://example.com/file2.txt"]
+urls = ["https://www.financnik.cz/exe/techlab/FASTSHORT.csv?id=14781:eb4d1c44d1087dd2b176be09b45859904daf1946ada3f1b23c0b7e8e0860e10b",
+"https://www.financnik.cz/exe/techlab/MONDAYBUYER.csv?id=14781:eb4d1c44d1087dd2b176be09b45859904daf1946ada3f1b23c0b7e8e0860e10b",
+"https://www.financnik.cz/exe/techlab/MOPULLIMIT.csv?id=14781:eb4d1c44d1087dd2b176be09b45859904daf1946ada3f1b23c0b7e8e0860e10b",
+"https://www.financnik.cz/exe/techlab/SMOPRO.csv?id=14781:eb4d1c44d1087dd2b176be09b45859904daf1946ada3f1b23c0b7e8e0860e10b"]
+
 downloaded_files = []
 
 for i, url in enumerate(urls):
